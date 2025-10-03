1e2ab06bb0b1456cc34ba2c18be3eb08362a9530
Cyberbot501 <softwareengineerjoycy@gmail.com>
2025-10-03 11:35:43 +0100

    done

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indwcm5kdXdoZW95bm1zb3J6em11Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk0NDUyNDMsImV4cCI6MjA3NTAyMTI0M30.p8Ykav7PGEdIeahBLkN_bBQkwxG9RfXAmbv0g_px0lo"
+const supabaseAnonKey = "https://wprnduwheoynmsorzzmu.supabase.co"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
