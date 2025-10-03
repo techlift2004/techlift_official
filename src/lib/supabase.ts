d92a473a4133a04c9378563d1f0ca90d52355cd0
Cyberbot501 <softwareengineerjoycy@gmail.com>
2025-10-03 11:39:14 +0100

    done

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indwcm5kdXdoZW95bm1zb3J6em11Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk0NDUyNDMsImV4cCI6MjA3NTAyMTI0M30.p8Ykav7PGEdIeahBLkN_bBQkwxG9RfXAmbv0g_px0lo"
-const supabaseAnonKey = "https://wprnduwheoynmsorzzmu.supabase.co"
+const supabaseUrl = "https://wprnduwheoynmsorzzmu.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indwcm5kdXdoZW95bm1zb3J6em11Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk0NDUyNDMsImV4cCI6MjA3NTAyMTI0M30.p8Ykav7PGEdIeahBLkN_bBQkwxG9RfXAmbv0g_px0lo"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
